73bdbecac58979928fc76bb23b47ceee49b2f952
Shariful Islam <sharifrony725@gmail.com>
2022-12-06 08:31:29 +0600

    C Program to print all alphabet from A to Z using while loop

---
+++ homework_4.c
@@ -0,0 +1,17 @@
+/*
+Name : Shariful Islam
+ID: 222010134
+Section: C
+HomeWork-3
+*C Program to print all alphabet from A to Z using while loop.
+*/
+#include<stdio.h>
+int main(){
+    char alphabet = 'A';
+    while(alphabet <= 'Z')
+    {
+        printf("%c ",alphabet);
+        alphabet++;
+    }
+    return 0;
+}
