9e92f0cc279edcda30da6881f7b2d051b92fa2d5
Shariful Islam <sharifrony725@gmail.com>
2022-12-25 21:51:20 +0600

    homework-10

---
+++ homework_10.c
@@ -0,0 +1,16 @@
+/*
+Name : Shariful Islam
+ID: 222010134
+Section: C
+HomeWork-10
+*/
+#include <stdio.h>
+int main() {
+	int i,j;
+	for (i=0; i<5; i++) {
+		for (j=0; j<5; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
